f052b9bc4341f7a86c02252c97f21f23bc00e325
maxscheinberg <75459032+maxscheinberg@users.noreply.github.com>
2026-02-10 18:09:41 -0500

    changed shape of adder/subtractor

--- color_adder.gd
+++ color_adder.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 @export var trigger_color: Player.PlayerColor = Player.PlayerColor.BLUE
 
-@onready var color_rect: ColorRect = $ColorRect
+@onready var polygon: Polygon2D = $Polygon2D
 
 func _ready() -> void:
 	_apply_visual_color()
@@ -18,18 +18,18 @@ func _on_body_entered(body: Node2D) -> void:
 func _apply_visual_color() -> void:
 	match trigger_color:
 		Player.PlayerColor.RED:
-			color_rect.color = Color(1, 0, 0, 1)
+			polygon.color = Color(1, 0, 0, 1)
 		Player.PlayerColor.BLUE:
-			color_rect.color = Color(0, 0, 1, 1)
+			polygon.color = Color(0, 0, 1, 1)
 		Player.PlayerColor.YELLOW:
-			color_rect.color = Color(1, 1, 0, 1)
+			polygon.color = Color(1, 1, 0, 1)
 		Player.PlayerColor.GREEN:
-			color_rect.color = Color(0, 1, 0, 1)
+			polygon.color = Color(0, 1, 0, 1)
 		Player.PlayerColor.PURPLE:
-			color_rect.color = Color(0.5, 0, 0.5, 1)
+			polygon.color = Color(0.5, 0, 0.5, 1)
 		Player.PlayerColor.ORANGE:
-			color_rect.color = Color(1, 0.5, 0, 1)
+			polygon.color = Color(1, 0.5, 0, 1)
 		Player.PlayerColor.WHITE:
-			color_rect.color = Color(1, 1, 1, 1)
+			polygon.color = Color(1, 1, 1, 1)
 		Player.PlayerColor.PINK:
-			color_rect.color = Color(1, 0.4, 0.7, 1)
+			polygon.color = Color(1, 0.4, 0.7, 1)
